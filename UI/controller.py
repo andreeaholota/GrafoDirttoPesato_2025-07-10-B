849edc7f536612d49dd5c6fe3a8f7288bf2a32d4
Giuseppe Averta <g.averta3@gmail.com>
2025-07-09 11:54:12 +0200

    first commit

---
+++ UI/controller.py
@@ -0,0 +1,33 @@
+import datetime
+
+import flet as ft
+
+
+class Controller:
+    def __init__(self, view, model):
+        # the view, with the graphical elements of the UI
+        self._view = view
+        # the model, which implements the logic of the program and holds the data
+        self._model = model
+
+    def handleCreaGrafo(self, e):
+        pass
+
+    def handleBestProdotti(self, e):
+        pass
+
+    def handleCercaCammino(self, e):
+        pass
+
+
+
+    def setDates(self):
+        first, last = self._model.getDateRange()
+
+        self._view._dp1.first_date = datetime.date(first.year, first.month, first.day)
+        self._view._dp1.last_date = datetime.date(last.year, last.month, last.day)
+        self._view._dp1.current_date = datetime.date(first.year, first.month, first.day)
+
+        self._view._dp2.first_date = datetime.date(first.year, first.month, first.day)
+        self._view._dp2.last_date = datetime.date(last.year, last.month, last.day)
+        self._view._dp2.current_date = datetime.date(last.year, last.month, last.day)
---
+++ database/DAO.py
@@ -0,0 +1,28 @@
+from database.DB_connect import DBConnect
+
+
+class DAO():
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def getDateRange():
+
+        conn = DBConnect.get_connection()
+
+        results = []
+
+        cursor = conn.cursor(dictionary=True)
+        query = "SELECT distinct (order_date) from orders o order by order_date"
+
+        cursor.execute(query)
+
+        for row in cursor:
+            results.append(row["order_date"])
+
+        first = results[0]
+        last = results[-1]
+
+        cursor.close()
+        conn.close()
+        return first, last
